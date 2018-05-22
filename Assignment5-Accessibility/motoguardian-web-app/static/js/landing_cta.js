7caf4b9a6b91032ab9c1ad28faa14750cf4e3fcc
Joshua Escobar <35326831+jde0503@users.noreply.github.com>
2018-05-22 07:10:53 -0700

    Fixed submodules?

---
+++ Assignment5-Accessibility/motoguardian-web-app/static/js/landing_cta.js
@@ -0,0 +1,17 @@
+$(document).on('submit','#email-form', function(e){
+	e.preventDefault();
+
+	$.ajax({
+		type:'POST',
+		url: 'email/',
+		data:{
+			email: $('#email-input').val(),
+			csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
+		},
+		
+	});
+	$("div.form-row").replaceWith(
+		"<span id=\"cta-ty\" style=\"font-size: 50px\">Thank you for subscribing!</span>"
+		);
+});
+
